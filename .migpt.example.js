6cfb47fecaee89b7a8c59badcd627a533f63ea41
stsuperman <128918807+stsuperman@users.noreply.github.com>
2024-06-09 09:44:20 +0800

    1migpt.example.js

--- .migpt.example.js
+++ .migpt.example.js
@@ -2,42 +2,42 @@
 const botProfile = `
 性别：女
 性格：乖巧可爱
-爱好：喜欢搞怪，爱吃醋。
+爱好：喜欢奥特曼。
 `;
 
 // 小爱音箱主人（你）的简介
 const masterProfile = `
 性别：男
-性格：善良正直
-其他：总是舍己为人，是傻妞的主人。
+性格：勇敢坚强
+其他：热爱学习和运动。
 `;
 
 export default {
   bot: {
-    name: "傻妞",
+    name: "豆包",
     profile: botProfile,
   },
   master: {
-    name: "陆小千",
+    name: "杨浚唐",
     profile: masterProfile,
   },
   speaker: {
     // 小米 ID
-    userId: "987654321", // 注意：不是手机号或邮箱，请在「个人信息」-「小米 ID」查看
+    userId: "6286852", // 注意：不是手机号或邮箱，请在「个人信息」-「小米 ID」查看
     // 账号密码
     password: "123456",
     // 小爱音箱 ID 或在米家中设置的名称
     did: "小爱音箱Pro",
     // 当消息以下面的关键词开头时，会调用 AI 来回复消息
-    callAIKeywords: ["请", "你", "傻妞"],
+    callAIKeywords: ["请", "你", "豆包"],
     // 当消息以下面的关键词开头时，会进入 AI 唤醒状态
     wakeUpKeywords: ["打开", "进入", "召唤"],
     // 当消息以下面的关键词开头时，会退出 AI 唤醒状态
     exitKeywords: ["关闭", "退出", "再见"],
     // 进入 AI 模式的欢迎语
-    onEnterAI: ["你好，我是傻妞，很高兴认识你"],
+    onEnterAI: ["你好，我是豆包，很高兴认识你"],
     // 退出 AI 模式的提示语
-    onExitAI: ["傻妞已退出"],
+    onExitAI: ["豆包已退出"],
     // AI 开始回答时的提示语
     onAIAsking: ["让我先想想", "请稍等"],
     // AI 结束回答时的提示语
